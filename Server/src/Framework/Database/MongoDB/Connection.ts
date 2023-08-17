f4ec288ab1ceebd9b07c9d93c0ab503ef93e8375
Fasal Thoufeeq <fasaltq681@gmail.com>
2023-08-17 23:41:27 +0530

    mongo commit

--- Server/src/Framework/Database/MongoDB/Connection.ts
+++ Server/src/Framework/Database/MongoDB/Connection.ts
@@ -5,10 +5,10 @@ mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
     try {
-      if (!configKeys.MONGODB_URL) {
-        throw new Error("MongoDB URL is not defined in configKeys");
-      }
-      await mongoose.connect(configKeys.MONGODB_URL, {});
+      // if (!configKeys.MONGODB_URL) {
+      //   throw new Error("MongoDB URL is not defined in configKeys");
+      // }
+      await mongoose.connect("mongodb+srv://fasaltq681:Fasal123@careerlink.mhwif17.mongodb.net/", {});
       console.log(`Database connected successfully`);
     } catch (error) {
       console.log(error);
