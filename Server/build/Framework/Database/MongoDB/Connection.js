dedf322d22dd1ee6d0b165c0b7f080c841ac5c72
Fasal Thoufeeq <fasaltq681@gmail.com>
2023-08-17 23:52:40 +0530

    mongo commit2

--- Server/build/Framework/Database/MongoDB/Connection.js
+++ Server/build/Framework/Database/MongoDB/Connection.js
@@ -4,12 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
+const config_1 = __importDefault(require("../../../config"));
 mongoose_1.default.set('strictQuery', true);
 const connectDB = async () => {
     try {
-        // if (!configKeys.MONGODB_URL) {
-        //   throw new Error("MongoDB URL is not defined in configKeys");
-        // }
+        if (!config_1.default.MONGODB_URL) {
+            throw new Error("MongoDB URL is not defined in configKeys");
+        }
         await mongoose_1.default.connect("mongodb+srv://fasaltq681:Fasal123@careerlink.mhwif17.mongodb.net/", {});
         console.log(`Database connected successfully`);
     }
--- Server/src/Framework/Database/MongoDB/Connection.ts
+++ Server/src/Framework/Database/MongoDB/Connection.ts
@@ -5,9 +5,9 @@ mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
     try {
-      // if (!configKeys.MONGODB_URL) {
-      //   throw new Error("MongoDB URL is not defined in configKeys");
-      // }
+      if (!configKeys.MONGODB_URL) {
+        throw new Error("MongoDB URL is not defined in configKeys");
+      }
       await mongoose.connect("mongodb+srv://fasaltq681:Fasal123@careerlink.mhwif17.mongodb.net/", {});
       console.log(`Database connected successfully`);
     } catch (error) {
